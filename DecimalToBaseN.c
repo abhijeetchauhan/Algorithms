da4a406f049db4942fba4e2058f266d3c8eb1624
supernoobabhi <abhijeet_chauhan@yahoo.co.in>
2015-08-05 20:19:32 +0530

    Modular Expo algo added

--- DecimalToBaseN.c
+++ DecimalToBaseN.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include "myheader.h"
 void main(){
-  int decimal,base,*p,i;
-  printf("Enter no in decimal: ");
-  scanf("%d",&decimal);
-  printf("Enter base to be converted in: ");
-  scanf("%d",&base);
-  p=DecimalToBaseN(decimal,base);
-  for(i=*p;i>0;i--){
-  printf("%d",*(p+i));
-  }
+  int power,base,divisor;
+  printf("Enter base,power and divisor");
+  scanf("%d%d%d",&base,&power,&divisor);
+  printf("The remainder is %d\n",ModExpo(base,power,divisor));
 }
--- myheader.h
+++ myheader.h
@@ -1,4 +1,21 @@
 int * DecimalToBaseN(int no,int base){
+  int output[100],reverse[100];
+  int *out;
+  out=reverse;
+  int i=1;
+  while(no){
+    output[i]=no%base;
+    no=no/base;
+    i++;
+  }
+  output[0]=i;
+  reverse[0]=i;
+  for(i=1;i<output[0];i++){
+    reverse[i]=output[output[0]-i];
+  }
+  return out;
+}
+int * DecimalToBaseNr(int no,int base){
   int output[100];
   int *out;
   out=output;
@@ -8,7 +25,17 @@ int * DecimalToBaseN(int no,int base){
     no=no/base;
     i++;
   }
-  i--;
   output[0]=i;
   return out;
+}
+int ModExpo(int base,int decimalpower,int divisor){
+  int rem=1,i;
+  int *bin=DecimalToBaseNr(decimalpower,2);
+  int power=base%divisor;
+  for(i=1;i<bin[0];i++){
+    if(bin[i]==1)
+      rem=(rem*power)%divisor;
+    power=(power*power)%divisor;
+  }
+  return rem;
 }
