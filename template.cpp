4ad645d979c63a307b4e3536153c9271e1e956db
supernoobabhi <abhijeet_chauhan@yahoo.co.in>
2016-01-12 17:26:45 +0530

    template added

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pll pair<long long int,long long int>
+#define f first
+#define s second
+using namespace std;
+
+int main(){
+  ll start_s=clock();
+  ll stop_s=clock();
+  cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << "\n";
+}
