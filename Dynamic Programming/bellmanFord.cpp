50f9a716f7bb9c0c61eac9b917153b75c36c6cb9
supernoobabhi <abhijeet_chauhan@yahoo.co.in>
2016-01-14 13:21:17 +0530

    Bellman Ford optimised

--- Dynamic Programming/bellmanFord.cpp
+++ Dynamic Programming/bellmanFord.cpp
@@ -9,7 +9,7 @@ vector <pll> graph[1001];
 ll ans[1001][1001];
 int main(){
   ll start_s=clock();
-  ll nodes,egdes,start,x,y,w;
+  ll nodes,egdes,start,x,y,w,flag=0,stop,size;
   scanf("%lld%lld",&nodes,&egdes);
   for(ll i=0;i<egdes;i++){
     scanf("%lld%lld%lld",&x,&y,&w);
@@ -23,19 +23,31 @@ int main(){
   for(ll i=1;i<nodes;i++){
     for(ll j=1;j<=nodes;j++){
       ll temp=LLONG_MAX;
+      size=0;
       for(auto it:graph[j]){
         //ans[i][it]=min(a[i-1][it],)
         if(ans[i-1][it.first]==LLONG_MAX)continue;
         temp=min(temp,ans[i-1][it.first]+it.second);
       }
       ans[i][j]=min(temp,ans[i-1][j]);
+      if(ans[i-1][j]==ans[i][j]){
+        size++;
+      }
     }
+    if(size==nodes){
+      flag=1;
+      stop=i;
+      break;
+    }
+  }
+  if(flag!=1){
+    stop=nodes-1;
   }
   // Outputing
   for(ll i=1;i<=nodes;i++){
-    printf("%lld ",ans[nodes-1][i]);
+    printf("%lld ",ans[stop][i]);
   }
-  printf("\n");
+  printf("\n%lld\n",stop);
   ll stop_s=clock();
   cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << "\n";
 }
