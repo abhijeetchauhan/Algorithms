0bc427dfdbb258cbf5d3f87df0dd6660a9c5885e
supernoobabhi <abhijeet_chauhan@yahoo.co.in>
2016-09-14 08:59:02 +0530

    Implemented threads in java extending Thread

--- Java/TestThread.java
+++ Java/TestThread.java
@@ -25,11 +25,38 @@ class RunnableDemo implements Runnable{
     }
   }
 }
+class ThreadDemo extends Thread{
+  Thread t;
+  String threadname;
+  ThreadDemo(String s){
+    threadname=s;
+    System.out.println("Creating : "+s);
+  }
+  public void run(){
+    System.out.println("Running : "+threadname);
+    try{
+      for(int i=4;i>0;i--){
+        System.out.println("Thread : "+threadname+","+i);
+        Thread.sleep(50);
+      }
+    }catch(InterruptedException e){
+      System.out.println("Thread : "+threadname+"Interrupted");
+    }
+    System.out.println("Thread : "+threadname+"Exiting.");
+  }
+  public void start(){
+    System.out.println("Starting :"+threadname);
+    if(t==null){
+      t = new Thread(this,threadname);
+      t.start();
+    }
+  }
+}
 class TestThread{
   public static void main(String []g){
     RunnableDemo a = new RunnableDemo("First");
     a.start();
-    RunnableDemo b = new RunnableDemo("Second");
+    ThreadDemo b = new ThreadDemo("Second");
     b.start();
   }
 }
